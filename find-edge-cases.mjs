f3235090eba4bb4a86ba94096c0ae31dd650d9c5
Dan Fabulich <dan@fabulich.com>
2023-08-25 15:20:09 -0700

    generate CSV

--- find-edge-cases.mjs
+++ find-edge-cases.mjs
@@ -1,19 +1,17 @@
 import { readFileSync } from 'node:fs';
 
-const target = 95;
-const almostTarget = 94;
-
 const { data: features } = JSON.parse(readFileSync('caniuse/data.json', 'utf8'));
 
 const keystoneReleaseDates = JSON.parse(readFileSync('keystone-release-dates.json', 'utf8'));
 
-const results = Object.fromEntries(Object.keys(keystoneReleaseDates).filter(featureId => 
-    features[featureId].usage_perc_y < target && features[featureId].usage_perc_y > almostTarget
-).map(featureId => 
-    [featureId, {
-        usage_perc_y: features[featureId].usage_perc_y,
-        keystone_date: keystoneReleaseDates[featureId]
-    }]
-));
+const results = Object.keys(keystoneReleaseDates).map(featureId => 
+    [
+        featureId,
+        features[featureId].usage_perc_y,
+        keystoneReleaseDates[featureId]
+    ]
+);
 
-console.log(JSON.stringify(results, null, 2));
+console.log(`feature_id,marketshare,keystone_date`);
+
+console.log(results.map(row => row.join(',')).join('\n'));
